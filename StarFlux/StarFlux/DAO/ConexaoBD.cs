97643fc9ccedbcbee5703da8c87b1ccc563c7ef4
gscarabeli <gustavocorreiascarabeli@gmail.com>
2023-11-18 16:47:27 -0300

    Home e Sobre parte 2

--- StarFlux/StarFlux/DAO/ConexaoBD.cs
+++ StarFlux/StarFlux/DAO/ConexaoBD.cs
@@ -6,7 +6,8 @@ namespace StarFlux.DAO
     {
         public static SqlConnection GetConexao()
         {
-            string strCon = "Data Source=DAVI\\SQLEXPRESS;Database=StarFlux;Integrated Security=True;TrustServerCertificate=True;";
+            //string strCon = "Data Source=DAVI\\SQLEXPRESS;Database=StarFlux;Integrated Security=True;TrustServerCertificate=True;";
+            string strCon = "Data Source=DELL;Initial Catalog=StarFlux;Integrated Security=SSPI;";
             SqlConnection conexao = new SqlConnection(strCon);
             conexao.Open();
             return conexao;
